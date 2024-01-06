4469356dc423a0cd77406cdba416e0dd74120e0f
Funduchka <kotikbryansk@gmail.com>
2024-01-06 12:25:11 +0300

    HW03 minor fix - exit func add

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Funduchka/otus_go_basic_hw/hw03_chessboard/board"
 )
@@ -12,6 +13,7 @@ func main() {
 	_, err := fmt.Scanf("%d", &size)
 	if err != nil {
 		fmt.Println("Incorrect input, please try again")
+		os.Exit(1)
 	}
 
 	board.DrawBoard(size)
